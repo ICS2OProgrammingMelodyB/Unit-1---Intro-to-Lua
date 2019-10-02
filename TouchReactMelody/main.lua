abbc1e6b7882ba21e911e9f2ecc34f6ed5fe96e7
melody2005 <55598373+melody2005@users.noreply.github.com>
2019-10-02 15:03:53 -0400

    finished project

--- TouchReactMelody/main.lua
+++ TouchReactMelody/main.lua
@@ -4,7 +4,7 @@
 -- This program does something when I click on the button
 
 -- set bachground colour
-display.setDefault ("background", 50/255, 255/255, 50/255 )
+display.setDefault ("background", 178/255, 50/255, 100/255 )
 
 --hide status bar
 display.setStatusBar(display.HiddenStatusBar)
@@ -14,20 +14,6 @@ local blueButton = display.newImageRect("Images/Fast Button Inactive@2x.png", 19
 blueButton.x = display.contentWidth/2
 blueButton.y = display.contentHeight/2
 blueButton.isVisible = true 
-
-
--- Completion listener function(I got it from https://docs.coronalabs.com/api/library/audio/loadSound.html)
-local function CorrectAnswer( event )
-    print( "CorrectAnswer Finished on channel", event.channel )
-    if ( event.completed ) then
-        print( "CorrectAnswer completed playback naturally" )
-    else
-        print( "CorrectAnswer was stopped before completion" )
-    end
-end
- 
--- Load two audio streams and one sound(I got it from the same wedsite but I made some changes.)
-local backgroundMusic = audio.loadStream( "Images/Correct Answer Sound Effect.MP3" )
 
 --create red button, set its positions and make it visible
 local redButton = display.newImageRect("Images/Fast Button Active@2x.png", 198, 98)
@@ -39,9 +25,18 @@ redButton.isVisible = false
 local textObject = display.newText("Clicked!", 0, 0, nil, 50)
 textObject.x = display.contentWidth/2
 textObject.y = display.contentHeight/3
-textObject:setTextColor(5/255, 120/255, 255/255)
+textObject:setTextColor(255/255, 120/255, 5/255)
 textObject.isVisible = false
 
+--create image, set its position and make it invisible
+local checkMark = display.newImageRect("Images/checkmark.png", 150, 150)
+checkMark.x = display.contentWidth/2
+checkMark.y = 600
+checkMark.isVisible = false
+
+--create sound(I got this code from https://docs.coronalabs.com/api/library/audio/play.html)
+local correctSound = audio.loadSound( "Sound/Correct Answer Sound Effect.MP3" )
+ 
 -- Function: BlueButtonListener
 -- Input: touch listener
 -- Output: none
@@ -52,17 +47,16 @@ local function BlueButtonListener(touch)
 		blueButton.isVisible = false
 		redButton.isVisible = true
 		textObject.isVisible = true
-		backgroundMusic1 = true
-		-- Play the background music on channel 1, and fades out in 3 seconds 
-        local backgroundMusicChannel = audio.play( backgroundMusic, { channel=1, loops=0, fadeout=3000 } )
-
-	end
+		checkMark.isVisible = true
+		-- Play the correct soud on any available channel
+        local correctSoundChannel = audio.play( correctSound )
+    end
 
 	if (touch.phase == "ended") then
 		blueButton.isVisible = true
 		redButton.isVisible = false
 		textObject.isVisible = false
-		backgroundMusic1 = false
+		checkMark.isVisible = false
 	end
 end
 
@@ -76,15 +70,16 @@ blueButton:addEventListener("touch", BlueButtonListener)
 -- and make the Red button disappear
 local function RedButtonListener(touch)
 	if (touch.phase == "began") then
-		redButton.isVisible = false
-		blueButton.isVisible = true
-		textObject.isVisible = true
-	end
-
-	if (touch.phase == "ended") then
 		redButton.isVisible = true
 		blueButton.isVisible = false
+		textObject.isVisible = true
+		checkMark.isVisible = true
+    end
+	if (touch.phase == "ended") then
+		redButton.isVisible = false
+		blueButton.isVisible = true
 		textObject.isVisible = false
+		checkMark.isVisible = false
 	end
 end
 
