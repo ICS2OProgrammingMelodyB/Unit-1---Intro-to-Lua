ce098eb8e85e1a01bba4253d0285c0651a588563
melody2005 <55598373+melody2005@users.noreply.github.com>
2019-10-17 14:49:35 -0400

    added some work

--- PhysicsAndCollisionsMelody/main.lua
+++ PhysicsAndCollisionsMelody/main.lua
@@ -1,7 +1,7 @@
 -- Title: PhysicsAndCollisions
 -- Name: Melody Berhane
 -- Course: ICS2O
--- This program...
+-- This program displays four balls that roll across the screen.
 ----------------------------------------------------------------------------
 
 --hide the status bar
@@ -12,6 +12,13 @@ local physics = require("physics")
 
 -- start physics
 physics.start()
+
+----------------------------------------------------
+--SOUNDS
+----------------------------------------------------
+local backgroundSound = audio.loadSound( "Sound/backgroundMusic.MP3" )
+local backgroundSoundChannel = audio.play( backgroundSound )
+
 -----------------------------------------------------
 --OBJECTS
 -----------------------------------------------------
@@ -27,22 +34,40 @@ ground.width = display.contentWidth
 physics.addBody(ground, "static", {friction=0.5, bounce=0.3})
 
 ---------------------------------------------------------
--- beam
-local beam = display.newImage("Images/beam.png", 0, 0)
+-- beam1
+local beam1 = display.newImage("Images/beam.png", 0, 0)
 -- set the x and y location of the ground
-beam.x = display.contentCenterX/5
-beam.y = display.contentCenterY*1.65
+beam1.x = display.contentCenterX/5
+beam1.y = display.contentCenterY*1.65
 --change the width to be half of the ipad width
-beam.width = display.contentWidth/2
-beam.height = display.contentHeight * 1/10
+beam1.width = display.contentWidth/2
+beam1.height = display.contentHeight * 1/10
 -- rotate the beam -60 degrees so its on an angle
-beam:rotate(45)
+beam1:rotate(45)
 
 -- send it to the back layer
-beam:toBack()
+beam1:toBack()
 
 --add to physics
-physics.addBody(beam, "static", {friction=0.5, bounce=0.3})
+physics.addBody(beam1, "static", {friction=0.5, bounce=0.3})
+
+--------------------------------------------------------------------------------------
+-- beam1
+local beam2 = display.newImage("Images/beam.png", 0, 0)
+-- set the x and y location of the ground
+beam2.x = 900
+beam2.y = display.contentCenterY*1.65
+--change the width to be half of the ipad width
+beam2.width = display.contentWidth/2
+beam2.height = display.contentHeight * 1/10
+-- rotate the beam 60 degrees so its on an angle
+beam2:rotate(-45)
+
+-- send it to the back layer
+beam2:toBack()
+
+--add to physics
+physics.addBody(beam2, "static", {friction=0.5, bounce=0.3})
 
 --------------------------------------------------------------
 -- background
@@ -67,9 +92,10 @@ background:toBack()
 local function firstBall()
 	-- creating first ball
 	local ball1 = display.newImage("Images/super_ball.png", 0, 0)
+	ball1:scale(0.5, 0.5)
 
 	-- add to physics
-	physics.addBody(ball1, {density=1.0, friction=0.5, bounce=0.3, radius=25})
+	physics.addBody(ball1, {density=1.0, friction=0.1, bounce=0.3, radius=12.5})
 end
 
 -----------------------------------------------------------------------------
@@ -78,12 +104,11 @@ end
 local function secondBall()
 	-- creating second ball
 	local ball2 = display.newImage("Images/super_ball.png", 0, 0)
-	ball2.width = display.contentWidth * 1/30
-	ball2.height = display.contentHeight * 1/28
+	
 
 
 	-- add to physics
-	physics.addBody(ball2, {density=1.0, friction=0.5, bounce=0.3, radius=12.5})
+	physics.addBody(ball2, {density=1.0, friction=0.1, bounce=0.3, radius=25})
 
 end
 
@@ -93,12 +118,12 @@ end
 local function thirdBall()
 	-- creating second ball
 	local ball3 = display.newImage("Images/super_ball.png", 0, 0)
-	ball3.width = display.contentWidth * 1/15
-	ball3.height = display.contentHeight * 1/13
+	ball3:scale(2, 2)
+
 
 
 	-- add to physics
-	physics.addBody(ball3, {density=1.0, friction=0.5, bounce=0.3, radius=12.5})
+	physics.addBody(ball3, {density=1.0, friction=0.1, bounce=0.3, radius=50})
 
 end
 
@@ -108,23 +133,18 @@ end
 local function fourthBall()
 	-- creating second ball
 	local ball4 = display.newImage("Images/super_ball.png", 0, 0)
-	ball4.width = display.contentWidth * 1/7
-	ball4.height = display.contentHeight * 1/5
+	ball4:scale(3, 3)
+
 
 
 	-- add to physics
-	physics.addBody(ball4, {density=1.0, friction=0.5, bounce=0.3, radius=12.5})
+	physics.addBody(ball4, {density=1.0, friction=0.1, bounce=0.3, radius=75})
 
 end
 
 
 
 
-local leftWall = display.newImageRect("Platform.png", 320, 200)
-leftWall.x = display.contentWidth - 450
-leftWall.y = display.contentHeight - 40
-leftWall.alpha = 0
-physics.addBody(leftWall, "static", {bouce = 0})
 
 
 
